f27467ea332108c38454eb6ceab307aa9af829e8
Jeremy Purser <jeremypurser@gmail.com>
2019-09-04 23:36:38 -0400

    Add styled components alias

--- webpack.config.js
+++ webpack.config.js
@@ -1,144 +1,61 @@
 const path = require('path');
 const DIST_DIR = path.join(__dirname, '/public');
 
-module.exports = [
-  {
-    entry: {
-      more: path.join(__dirname, '../more_like_this_service/client/src/index.jsx'),
-    },
-    output: {
-      filename: '[name].js',
-      path: DIST_DIR
-    },
-    module: {
-      rules: [
-        {
-          test: [/\.(js|jsx)$/],
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env']
-            }
+module.exports = {
+  entry: {
+    more: path.join(__dirname, '../more_like_this_service/client/src/index.jsx'),
+    photos: path.join(__dirname, '../Photo-Carousel/client/src/index.jsx'),
+    itemoverview: path.join(__dirname, '../item-data-service/client/src/index.jsx')
+  },
+  output: {
+    filename: '[name].js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.(js|jsx)$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
           }
-        },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            { loader: 'style-loader' },
-            {
-              loader:'css-loader',
-              options: {
-                modules: true
-              }
-            },
-            { loader: 'sass-loader' }
-          ]
         }
-      ]
-    },
-    optimization: {
-      runtimeChunk: {
-        name: 'vendors'
       },
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all'
-          }
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader:'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          { loader: 'sass-loader' }
+        ]
+      }
+    ]
+  },
+  optimization: {
+    runtimeChunk: {
+      name: 'vendors'
+    },
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
         }
       }
     }
   },
-
-  {
-    entry: {
-      photos: path.join(__dirname, '../Photo-Carousel/client/src/index.jsx'),
-    },
-    module: {
-      rules: [
-        {
-          test: [/\.(js|jsx)$/],
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env']
-            }
-          }
-        },
-        {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            { loader: 'style-loader' },
-            {
-              loader:'css-loader',
-              options: {
-                modules: true
-              }
-            },
-            { loader: 'sass-loader' }
-          ]
-        }
-      ]
-    },
-    output: {
-      filename: '[name].js',
-      path: DIST_DIR
-    },
-    optimization: {
-      runtimeChunk: {
-        name: 'vendors'
-      },
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all'
-          }
-        }
-      }
-    }
-  },
-
-  {
-    entry: {
-      itemoverview: path.join(__dirname, '../item-data-service/client/src/index.jsx'),
-    },
-    module: {
-      rules: [
-        {
-          test: [/\.jsx$/],
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-react', '@babel/preset-env']
-            }
-          }
-        }
-      ]
-    },
-    output: {
-      filename: '[name].js',
-      path: DIST_DIR
-    },
-    optimization: {
-      runtimeChunk: {
-        name: 'vendors'
-      },
-      splitChunks: {
-        cacheGroups: {
-          commons: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            chunks: 'all'
-          }
-        }
-      }
+  resolve: {
+    alias: {
+      'styled-components': path.resolve(__dirname, 'node_modules', 'styled-components'),
+      'style-loader': path.resolve(__dirname, 'node_modules', 'styled-components')
     }
   }
-];
+};
